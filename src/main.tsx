db5f6cbdd8942b6651c6968fa2c119e25a03199a
Artm-porjad <Artm-porjad@mail.ru>
2023-03-24 22:37:08 +0300

    first version of the table

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { ModalState } from './context/ModalContext';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <BrowserRouter>
+    <ModalState>
+      <App />
+    </ModalState>
+  </BrowserRouter>
+);
